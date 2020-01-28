5b91ecbe9d691fd4a2e9317aade1f47cba1954bc
tacocodebs <60405089+tacocodebs@users.noreply.github.com>
2020-01-28 14:20:59 -0500

    add

---
+++ Logger.cs
@@ -0,0 +1,9 @@
+﻿using IPALogger = IPA.Logging.Logger;
+
+namespace BSPlugin1
+{
+    internal static class Logger
+    {
+        internal static IPALogger log { get; set; }
+    }
+}
---
+++ PluginConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace BSPlugin1
+{
+    internal class PluginConfig
+    {
+        public bool RegenerateConfig = true;
+        public string text = "";
+
+    }
+}
